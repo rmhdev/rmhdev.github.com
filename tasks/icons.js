dbfcead7557cb1878889771271236bc24ac49680
Rober MH <rmh.dev@gmail.com>
2018-09-15 19:37:43 +0700

    automate icon regeneration

--- tasks/icons.js
+++ tasks/icons.js
@@ -3,6 +3,7 @@ const sharp = require('sharp');
 var icons = [
     {size: 32, name: "favicon.ico"},
     {size: 96, name: "favicon.png"},
+    {size: 144, name: "touchicon.png"},
     {size: 192, name: "icon-192x192.jpg"},
     {size: 512, name: "icon-512x512.jpg"}
 ];
@@ -15,6 +16,20 @@ for (let i = 0; i < icons.length; i++) {
         .resize(icon.size, icon.size, {
             kernel: sharp.kernel.lanczos3,
             fastShrinkOnLoad: false
+        })
+        .jpeg({
+            force: false,
+            quality: 84,
+            progressive: true,
+            // only with mozjpeg:
+            trellisQuantisation: true,
+            overshootDeringing: true,
+            optimiseScans: true
+        })
+        .png({
+            force: false,
+            compressionLevel: 9,
+            progressive: true
         })
         .toFile(toFile)
         .then(function() {
