98b5ffa5753f2470087a36af25a85392717a930d
Rober MH <rmh.dev@gmail.com>
2018-09-16 22:46:22 +0700

    improve illustrations script

--- tasks/illustrations.js
+++ tasks/illustrations.js
@@ -1,36 +1,39 @@
+const path = require('path');
 const fs = require('fs');
 const sharp = require('sharp');
 
+let appDir = path.dirname(require.main.filename);
 
-var origin = '../misc/illustrations/';
-var destination = '../images/posts/';
-let maxSize = 700;
+let options = {
+    origin: appDir + '/../misc/illustrations/',
+    destination: appDir + '/../images/posts/',
+    maxSize: 700,
+    resize: {
+        kernel: sharp.kernel.lanczos3,
+        fastShrinkOnLoad: false
+    },
+    jpeg: {
+        quality: 84,
+        progressive: true,
+        // only with mozjpeg:
+        trellisQuantisation: true,
+        overshootDeringing: true,
+        optimiseScans: true
+    }
+};
 
-
-
-
-let files = fs.readdirSync(origin);
+let files = fs.readdirSync(options.origin);
 files.forEach(function(file) {
-    if (fs.statSync(origin + file).isDirectory()) {
+    if (fs.statSync(options.origin + file).isDirectory()) {
 
         let illustrationName = file + ".jpg";
-        let illustration = origin + file + "/" + illustrationName;
+        let illustration = options.origin + file + "/" + illustrationName;
         if (fs.statSync(illustration).isFile()) {
             sharp(illustration)
-                .resize(maxSize, maxSize, {
-                    kernel: sharp.kernel.lanczos3,
-                    fastShrinkOnLoad: false
-                })
+                .resize(options.maxSize, options.maxSize, options.resize)
                 .max()
-                .jpeg({
-                    quality: 84,
-                    progressive: true,
-                    // only with mozjpeg:
-                    trellisQuantisation: true,
-                    overshootDeringing: true,
-                    optimiseScans: true
-                })
-                .toFile(destination + illustrationName)
+                .jpeg(options.jpeg)
+                .toFile(options.destination + illustrationName)
                 .then(function() {
                     console.log(illustrationName + ": ok");
                 })
